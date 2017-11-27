a2477d69810e7451725bca22699d9b875ed84036
backja <backja@rose-hulman.edu>
2017-11-27 14:57:19 -0500

    Done

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# Done: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# Done: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -77,7 +77,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 3.
+# Done: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -88,10 +88,14 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+matt.left(45)
+matt.forward(25)
+matt.pen=rg.Pen('orange',30)
+matt.right(10)
+matt.backward(100)
 ########################################################################
 #
-# TODO: 4.
+# Done: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +113,15 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+jacob=rg.SimpleTurtle("turtle")
+jacob.pen=rg.Pen("green",10)
+jacob.speed=10
+jacob.forward(25)
+jacob.right(45)
+jacob.backward(35)
 ########################################################################
 #
-# TODO: 5.
+# Done: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
